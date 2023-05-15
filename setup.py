420256125fabb4d6e1b19bcf9f0fd39eaee8be58
Kentechx <kd_shen@163.com>
2023-05-16 05:41:13 +0800

    for publish

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='x-dgcnn',
+    packages=find_packages(),
+    version='0.0.1',
+    license='MIT',
+    description='X-DGCNN - Pytorch',
+    author='Kaidi Shen',
+    url='https://github.com/kentechx/x-dgcnn',
+    long_description_content_type='text/markdown',
+    keywords=[
+        '3D segmentation',
+        '3D classification',
+        'point cloud understanding',
+    ],
+    install_requires=[
+        'torch>=1.10',
+        'einops>=0.6.1'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
