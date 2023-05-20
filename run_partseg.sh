23637c5b5cdf0bd537a2f18ffadd3e5cc75d17e1
Kentechx <kd_shen@163.com>
2023-05-20 20:01:32 +0800

    update

--- run_partseg.sh
+++ run_partseg.sh
@@ -1,18 +1,18 @@
 # set exp_name and class_choice
-exp_name=partseg
-model=dgcnn
+exp_name=partseg_dgcnn
+model=xdgcnn_dgcnn
 echo "exp_name: $exp_name"
 echo "model: $model"
 
 # full dataset
 python main_partseg.py --exp_name=${exp_name} --model=${model}
 # eval full dataset
-python main_partseg.py --exp_name=${exp_name}_eval --eval=True --model_path=outputs/${exp_name}/models/model.t7
+python main_partseg.py --exp_name=${exp_name}_eval --eval=True --model_path=outputs/${exp_name}/models/model.t7 --model=${model}
 
 for choice in airplane bag cap car chair earphone guitar knife lamp laptop motor mug pistol rocket skateboard table
 do
     # train
-    python main_partseg.py --exp_name=${exp_name}_${choice} --class_choice=${choice}
+    python main_partseg.py --exp_name=${exp_name}_${choice} --class_choice=${choice} --batch_size=16 --model=${model}
     # eval
-    python main_partseg.py --exp_name=${exp_name}_${choice}_eval --class_choice=${choice} --eval=True --model_path=outputs/${exp_name}_${choice}/models/model.t7
+    python main_partseg.py --exp_name=${exp_name}_${choice}_eval --class_choice=${choice} --eval=True --model_path=outputs/${exp_name}_${choice}/models/model.t7 --model=${model}
 done
