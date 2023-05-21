742c861e7ee275312dad51d319164ea2d9b15ad3
Kentechx <kd_shen@163.com>
2023-05-21 15:44:27 +0800

    update

--- run_partseg.sh
+++ run_partseg.sh
@@ -1,18 +1,19 @@
 # set exp_name and class_choice
 exp_name=partseg_dgcnn
 model=xdgcnn_dgcnn
+dropout=0.1
 echo "exp_name: $exp_name"
 echo "model: $model"
 
 # full dataset
-python main_partseg.py --exp_name=${exp_name} --model=${model}
+python main_partseg.py --exp_name=${exp_name} --model=${model} --dropout=${dropout}
 # eval full dataset
 python main_partseg.py --exp_name=${exp_name}_eval --eval=True --model_path=outputs/${exp_name}/models/model.t7 --model=${model}
 
 for choice in airplane bag cap car chair earphone guitar knife lamp laptop motor mug pistol rocket skateboard table
 do
     # train
-    python main_partseg.py --exp_name=${exp_name}_${choice} --class_choice=${choice} --model=${model}
+    python main_partseg.py --exp_name=${exp_name}_${choice} --class_choice=${choice} --model=${model} --dropout=${dropout}
     # eval
     python main_partseg.py --exp_name=${exp_name}_${choice}_eval --class_choice=${choice} --eval=True --model_path=outputs/${exp_name}_${choice}/models/model.t7 --model=${model}
 done
