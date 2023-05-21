655ebb2b7cc52badd214e9c4f6bef2ae80426cce
Kentechx <kd_shen@163.com>
2023-05-22 02:41:21 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='x-dgcnn',
     packages=find_packages(),
-    version='0.0.4',
+    version='0.1.0',
     license='MIT',
     description='X-DGCNN - Pytorch',
     author='Kaidi Shen',
